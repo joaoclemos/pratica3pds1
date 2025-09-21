2ae209dde62e321fe615c7bc29f9fa24d38a060a
joaoclemos <joaoclemos22@gmail.com>
2025-09-21 15:37:22 -0300

    Q4 completa

--- questao4.c
+++ questao4.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <limits.h>
 
-void main (){
-    int minimo = SHRT_MIN;
-    long long int maximo = LLONG_MAX;
-    printf("menor: %d\nmaior: %lld", minimo, maximo);
+void main() {
+    printf("menor: %hd\nmaior: %hd\n", SHRT_MIN, SHRT_MAX);
+    printf("menor: %d\nmaior: %d\n", INT_MIN, INT_MAX);
+    printf("menor: %ld\nmaior: %ld\n", LONG_MIN, LONG_MAX);
+    printf("menor: %lld\nmaior: %lld\n", LLONG_MIN, LLONG_MAX);
+    printf("menor: 0\nmaior: %hu\n", USHRT_MAX);
+    printf("menor: 0\nmaior: %u\n", UINT_MAX);
+    printf("menor: 0\nmaior: %lu\n", ULONG_MAX);
+    printf("menor: 0\nmaior: %llu\n", ULLONG_MAX);
 }
