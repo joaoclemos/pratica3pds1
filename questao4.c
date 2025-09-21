f78f9e4e8547e861d39faddfbd632407d4ec74b4
joaoclemos <joaoclemos22@gmail.com>
2025-09-21 15:31:05 -0300

    Q4 parte2

--- questao4.c
+++ questao4.c
@@ -2,7 +2,7 @@
 #include <limits.h>
 
 void main (){
-    int minimo = INT_MIN;
-    int maximo = INT_MAX;
-    printf("menor: %d\nmaior: %d", minimo, maximo);
+    int minimo = SHRT_MIN;
+    long long int maximo = LLONG_MAX;
+    printf("menor: %d\nmaior: %lld", minimo, maximo);
 }
